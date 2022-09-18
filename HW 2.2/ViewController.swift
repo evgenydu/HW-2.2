5ca18b7e71eb94b31a1001c7918a2d0799443035
Evgeny D <ktrp@me.com>
2022-09-18 22:21:39 +0300

    Logic

--- HW 2.2/ViewController.swift
+++ HW 2.2/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    @IBOutlet var switchButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redView.layer.cornerRadius = 100
+        yellowView.layer.cornerRadius = 100
+        greenView.layer.cornerRadius = 100
+        switchButton.layer.cornerRadius = 10
+        
     }
 
-
+    @IBAction func switchColor(_ sender: Any) {
+        if switchButton.currentTitle == "START" {
+            redView.alpha = 1
+            switchButton.setTitle("NEXT", for: .normal)
+        } else {
+            if redView.alpha == 1 {
+                redView.alpha = 0.3
+                yellowView.alpha = 1
+                return
+            }
+            if yellowView.alpha == 1 {
+                yellowView.alpha = 0.3
+                greenView.alpha = 1
+                return
+            }
+            if greenView.alpha == 1 {
+                greenView.alpha = 0.3
+                redView.alpha = 1
+            }
+        }
+    }
 }
 
